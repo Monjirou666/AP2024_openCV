0ef0887485c56a912b7828e6352d88f74e2b53ab
yas0063 <yas@mech.eng.osaka-u.ac.jp>
2022-05-25 17:33:25 +0900

    Add files via upload

---
+++ 01/opencvTest_b_c.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+int main()
+{
+
+    cv::Mat src = cv::imread("lena.jpg");
+    cv::Mat dst;
+  
+    double alpha = 0.1; // contrast 
+    double beta = 1.0; // brightness
+     
+    src.convertTo(dst, -1, alpha, beta);  
+    cv::imshow("Display", dst);
+    
+    cv::waitKey(0);
+    return 0;
+}
+
+
---
+++ 01/opencvTest_blur.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main() {
+  cv::Mat src = cv::imread("lena.jpg");
+  cv::Mat dst;
+
+  int blur = 100;  // 変化の度合い 1-10
+
+  cv::medianBlur(src, dst, blur * 2 + 1);
+  cv::imshow("Display", dst);
+
+  cv::waitKey(0);
+
+  return 0;
+}
