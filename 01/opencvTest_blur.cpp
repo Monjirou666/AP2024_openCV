4c9851b5a07515947cac5faa15fae78821363cca
yas0063 <yas@mech.eng.osaka-u.ac.jp>
2022-06-08 08:02:12 +0900

    Update opencvTest_blur.cpp

--- 01/opencvTest_blur.cpp
+++ 01/opencvTest_blur.cpp
@@ -5,6 +5,8 @@ int main() {
   cv::Mat src = cv::imread("lena.jpg");
   cv::Mat dst;
 
+  cv::namedWindow("Display", cv::WINDOW_AUTOSIZE);
+  
   int blur = 100;  // 変化の度合い 1-10
 
   cv::medianBlur(src, dst, blur * 2 + 1);
